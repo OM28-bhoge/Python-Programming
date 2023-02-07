2c2f028e67564d185819aa23a223074d85c3ca72
Om <ombhoge1234@gmail.com>
2023-02-07 10:21:48 +0530

    Initial Commit

--- problem1.py
+++ problem1.py
@@ -1,8 +1,8 @@
 numbers = [10, 20, 30, 20, 40, 60, 20, 60, 80]
 count_dict = {}
 for number in numbers:
-    if number in count_dict:
-        count_dict[number] += 1
-    else :
+    if number not in count_dict:
         count_dict[number] = 1
+    else :
+        count_dict[number] += 1
 print(count_dict) 
